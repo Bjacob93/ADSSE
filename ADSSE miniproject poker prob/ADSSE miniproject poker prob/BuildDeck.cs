02e637306647a8964f8135aed9a859f8a68b0aba
Bjacob93 <Zeint@live.dk>
2018-05-24 10:48:56 +0200

    deck

---
+++ ADSSE miniproject poker prob/ADSSE miniproject poker prob/BuildDeck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSSE_miniproject_poker_prob
+{
+
+    public class BuildDeck
+    {
+        enum Suit { Diamonds, Hearts, Clubs, Spades}
+        
+        class Deck
+        {
+            public List<Card> Cards { get; set; }
+            public Deck()
+            {
+                Cards = new List<Card>();
+                foreach(Suit s in Enum.GetValues(typeof(Suit)))
+                {
+                    for(int i = 1; i < 13; i++)
+                    {
+                        Cards.Add(new Card(s, i));
+                    }
+                }
+            }
+        }
+
+        class Card
+        {
+            Suit suit { get; set; }
+            int rank { get; set; }
+            public Card(Suit s, int r)
+            {
+                suit = s;
+                rank = r;
+            }
+        }
+
+    }
+}
--- ADSSE miniproject poker prob/ADSSE miniproject poker prob/Form1.cs
+++ ADSSE miniproject poker prob/ADSSE miniproject poker prob/Form1.cs
@@ -12,7 +12,7 @@ namespace ADSSE_miniproject_poker_prob
 {
     public partial class Form1 : Form
     {
-        Deck deck = new Deck();
+        BuildDeck deck = new BuildDeck();
 
         public Form1()
         {
@@ -21,8 +21,7 @@ namespace ADSSE_miniproject_poker_prob
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int s = (int)deck.randNum();
-            MessageBox.Show(Convert.ToString(s));
+            MessageBox.Show("hello");
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
