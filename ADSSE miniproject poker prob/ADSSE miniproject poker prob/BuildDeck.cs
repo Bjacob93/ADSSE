cccb77661fa4986663f2b0b8037735eeee96208e
Bjacob93 <Zeint@live.dk>
2018-05-24 12:45:00 +0200

    building deck

--- ADSSE miniproject poker prob/ADSSE miniproject poker prob/BuildDeck.cs
+++ ADSSE miniproject poker prob/ADSSE miniproject poker prob/BuildDeck.cs
@@ -7,13 +7,41 @@ using System.Threading.Tasks;
 namespace ADSSE_miniproject_poker_prob
 {
 
+
     public class BuildDeck
     {
-        enum Suit { Diamonds, Hearts, Clubs, Spades }
+        enum CardSuit { Diamonds, Hearts, Clubs, Spades }
 
-        class Deck
+        public class Card
         {
-            public List<Card> Cards { get; set; }
+            private CardSuit suit;
+            private int rank;
+
+            public Card(CardSuit suit, int rank)
+            {
+                this.suit = suit;
+                this.rank = rank;
+            }
+
+        }
+
+
+        public List<Card> myDeck()
+        {
+            List<Card> cards = new List<Card>();
+            foreach(CardSuit s in Enum.GetValues(typeof(CardSuit)))
+            {
+                for(int i = 1; i < 13; i++)
+                {
+                    cards.Add(new Card(s, i));
+                }
+            }
+            return cards;
+        }
+
+        /*class Deck
+        {
+            List<Card> Cards { get; set; }
             public Deck()
             {
                 Cards = new List<Card>();
@@ -25,18 +53,10 @@ namespace ADSSE_miniproject_poker_prob
                     }
                 }
             }
-        }
-
-        class Card
-        {
-            Suit suit { get; set; }
-            int rank { get; set; }
-            public Card(Suit s, int r)
+            public List<Card> MyDeck
             {
-                suit = s;
-                rank = r;
+                get { return Cards; }
             }
-        }
-
+        }*/
     }
 }
