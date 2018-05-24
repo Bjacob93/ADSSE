08ff906ea5de4dc321e31071c8e4d72b2dd67e47
Bjacob93 <Zeint@live.dk>
2018-05-24 10:27:40 +0200

    deck wip

--- ADSSE miniproject poker prob/ADSSE miniproject poker prob/Form1.cs
+++ ADSSE miniproject poker prob/ADSSE miniproject poker prob/Form1.cs
@@ -12,6 +12,8 @@ namespace ADSSE_miniproject_poker_prob
 {
     public partial class Form1 : Form
     {
+        Deck deck = new Deck();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,8 @@ namespace ADSSE_miniproject_poker_prob
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hallo");
+            int s = (int)deck.randNum();
+            MessageBox.Show(Convert.ToString(s));
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
