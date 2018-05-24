3378337d47b931d2299340c6dba8b848f7bf9d19
Bjacob93 <Zeint@live.dk>
2018-05-24 16:02:22 +0200

    handProbability started

---
+++ ADSSE miniproject poker prob/ADSSE miniproject poker prob/HandProbabilities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSSE_miniproject_poker_prob
+{
+    class HandProbabilities
+    {
+
+        List<BuildDeck.Card> GameDeck = new List<BuildDeck.Card>();
+
+        public float Probabilities(List<BuildDeck.Card> currentCards)
+        {
+            bool pair;
+            float pairProbability = 0, twoPairProbability = 0;
+            if(currentCards.Count == 2)
+            {
+                foreach(BuildDeck.Card c in currentCards){
+                    foreach(BuildDeck.Card d in currentCards){
+                        if(c != d){
+                            if (c.rank == d.rank){
+                                pair = true;
+                                pairProbability = 100;
+                            }else
+                            {
+                                pairProbability = ((6 / 50) * 100);
+                            }
+                        }
+                    }
+                }
+            }
+            return pairProbability;
+        }
+    }
+}
